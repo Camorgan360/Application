0feec6a29240c1128e0c9c897a488da307350f28
Pooks <Ca.morgan360@gmail.com>
2019-05-01 21:21:34 -0700

    updated tests

---
+++ test/make-applicant.test.js
@@ -0,0 +1,28 @@
+// import
+const test = QUnit.test;
+test('time to test a function', function(assert) {
+
+    const expected = {
+        name:
+        age:
+        appSkills:
+        tall:
+        role:
+        motivation:
+
+
+    };
+    
+    const formData = new FormData();
+    formData.set('full-name', expected.name);
+    formData.set('age', expected.age);
+    formData.set('app-skills', expected.appSkills[0]);
+    formData.append('app-skills', expected.appSkills[0]);
+    formData.set('found', 'yes');
+    formData.set('motivation', expected.motivation);
+
+    const applicant = getApplicant(formData);
+
+    assert.deepEqual(applicant, expected);
+
+});
